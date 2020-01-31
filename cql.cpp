7fd95d30841f316f82d54b7e812ace5d4305d8ea
Charlie <charlie.hall184@gmail.com>
2020-01-31 02:47:17 -0500

    First Commit

---
+++ cql.cpp
@@ -0,0 +1,19 @@
+/****************************
+ * This program is a command line sql-like utility
+ * The purpose of this is to be an exercise for learning
+ * binary search trees and file io
+ 
+ *****************************/
+
+#include <iostream>
+
+struct Row {
+    long long index;
+    int data;
+};
+
+Row* search(int index);
+
+int main() {
+
+}
